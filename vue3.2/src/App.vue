eec4d7c880608de3b86765da907984319dc50816
shengxinjing <316783812@qq.com>
2022-01-16 21:30:05 +0800

    add mini vuex router

---
+++ vue3.2/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+let num = $ref(1)
+function add(){
+  num++
+}
+</script>
+
+<template>
+<h1 @click="add">{{num}}</h1>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
